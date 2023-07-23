b541f8be3faf1e8d3ed368d17c3c4bdec5aa4263
BuildTools <unconfigured@null.spigotmc.org>
2023-07-23 20:54:03 +0200

    small fixes

--- components/HamsterList.js
+++ components/HamsterList.js
@@ -11,7 +11,7 @@ import hamsterNftAbi from "../constants/BasicNft.json"
 
 const hamsterNftAddress = "0x5726c14663a1ead4a7d320e8a653c9710b2a2e89"
 
-export const selectedHamster
+export var selectedHamster
 
 export default function HamsterList({ tokenId }) {
     const { isWeb3Enabled, account } = useMoralis()
