d86e4db6372bc32e3865fe12a7f7a4f06bc223f1
BuildTools <unconfigured@null.spigotmc.org>
2023-07-23 21:15:48 +0200

    small fixes 9

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
 import { NotificationProvider } from "web3uikit"
-import Header from "../components/Header"
 
 function MyApp({ Component, pageProps }) {
     return (
         <MoralisProvider initializeOnMount={false}>
             <NotificationProvider>
-                <Header />
                 <Component {...pageProps} />
                 <style global jsx>{`
                     body {
