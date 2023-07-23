94dc0f5e66915126779eb3e234c10e53b79a09dc
BuildTools <unconfigured@null.spigotmc.org>
2023-07-23 23:38:52 +0200

    small fixes 29

--- components/EnemyList.js
+++ components/EnemyList.js
@@ -1,25 +1,15 @@
 import { useState, useEffect } from "react";
-import { useWeb3Contract, useMoralis } from "react-moralis";
+import { useMoralis } from "react-moralis";
 import { useHamsterContext } from "./HamsterContext";
 import { Dropdown, Avatar } from "web3uikit";
 import hamsterNftAbi from "../constants/BasicNft.json";
+
 const hamsterNftAddress = "0x5726c14663a1ead4a7d320e8a653c9710b2a2e89";
 
 export default function EnemyList() {
   const { isWeb3Enabled, account } = useMoralis();
   const { selectedHamsterTokenId } = useHamsterContext();
   const [enemyList, setEnemyList] = useState([]);
-
-  // Separate function to call the ownerOf function with a specific tokenId
-  async function getOwnerOf(tokenId) {
-    const { runContractFunction: ownerOf } = useWeb3Contract({
-      abi: hamsterNftAbi,
-      contractAddress: hamsterNftAddress,
-      functionName: "ownerOf",
-      params: { tokenId },
-    });
-    return ownerOf();
-  }
 
   useEffect(() => {
     if (isWeb3Enabled && selectedHamsterTokenId !== null) {
@@ -31,27 +21,42 @@ export default function EnemyList() {
     const enemies = [];
 
     for (let i = 0; i < 100; i++) {
-      const owner = await getOwnerOf(i);
-      console.log(owner, i)
-      if (owner !== account && owner !== "0x0000000000000000000000000000000000000000") {
-        enemies.push({
-          id: i,
-          label: `#${i}`,
-          prefix: (
-            <Avatar
-              avatarKey={3}
-              borderRadius={7.5}
-              fontSize={8}
-              size={24}
-              image={`../images/nobg/${i}.png`}
-              theme="image"
-            />
-          ),
-        });
+      try {
+        const owner = await getOwnerOf(i);
+        console.log(`Enemy #${i} Owner: ${owner}`);
+        if (owner !== account && owner !== "0x0000000000000000000000000000000000000000") {
+          enemies.push({
+            id: i,
+            label: `#${i}`,
+            prefix: (
+              <Avatar
+                avatarKey={3}
+                borderRadius={7.5}
+                fontSize={8}
+                size={24}
+                image={`../images/nobg/${i}.png`}
+                theme="image"
+              />
+            ),
+          });
+        }
+      } catch (error) {
+        console.error(`Error fetching owner of Enemy #${i}`, error);
       }
     }
 
     setEnemyList(enemies);
+  }
+
+  async function getOwnerOf(tokenId) {
+    const { runContractFunction } = useWeb3Contract({
+      abi: hamsterNftAbi,
+      contractAddress: hamsterNftAddress,
+      functionName: "ownerOf",
+      params: { tokenId },
+    });
+
+    return runContractFunction();
   }
 
   const [selectedEnemyTokenId, setSelectedEnemyTokenId] = useState(null);
--- components/HamsterList.js
+++ components/HamsterList.js
@@ -1,57 +1,61 @@
 import { useState, useEffect } from "react";
-import { useWeb3Contract, useMoralis } from "react-moralis";
+import { useMoralis } from "react-moralis";
 import { useHamsterContext } from "./HamsterContext";
 import { Dropdown, Avatar } from "web3uikit";
 import hamsterNftAbi from "../constants/BasicNft.json";
+
 const hamsterNftAddress = "0x5726c14663a1ead4a7d320e8a653c9710b2a2e89";
 
 export default function HamsterList() {
   const { selectedHamsterTokenId, setSelectedHamsterTokenId } = useHamsterContext();
-
   const { isWeb3Enabled, account } = useMoralis();
   const [hamsterList, setHamsterList] = useState([]);
-
-  // Separate function to call the ownerOf function with a specific tokenId
-  async function getOwnerOf(tokenId) {
-    const { runContractFunction: ownerOf } = useWeb3Contract({
-      abi: hamsterNftAbi,
-      contractAddress: hamsterNftAddress,
-      functionName: "ownerOf",
-      params: { tokenId },
-    });
-    return ownerOf();
-  }
-
-  async function updateUI() {
-    const newHamsterList = [];
-    for (let i = 0; i < 100; i++) {
-      const owner = await getOwnerOf(i);
-      console.log(owner, i)
-      if (owner === account) {
-        newHamsterList.push({
-          id: i,
-          label: `#${i}`,
-          prefix: (
-            <Avatar
-              avatarKey={3}
-              borderRadius={7.5}
-              fontSize={8}
-              size={24}
-              image={`../images/nobg/${i}.png`}
-              theme="image"
-            />
-          ),
-        });
-      }
-    }
-    setHamsterList(newHamsterList);
-  }
 
   useEffect(() => {
     if (isWeb3Enabled) {
       updateUI();
     }
   }, [isWeb3Enabled]);
+
+  async function updateUI() {
+    const newHamsterList = [];
+    for (let i = 0; i < 100; i++) {
+      try {
+        const owner = await getOwnerOf(i);
+        console.log(`Hamster #${i} Owner: ${owner}`);
+        if (owner === account) {
+          newHamsterList.push({
+            id: i,
+            label: `#${i}`,
+            prefix: (
+              <Avatar
+                avatarKey={3}
+                borderRadius={7.5}
+                fontSize={8}
+                size={24}
+                image={`../images/nobg/${i}.png`}
+                theme="image"
+              />
+            ),
+          });
+        }
+      } catch (error) {
+        console.error(`Error fetching owner of Hamster #${i}`, error);
+      }
+    }
+    setHamsterList(newHamsterList);
+  }
+
+  async function getOwnerOf(tokenId) {
+    const { runContractFunction } = useWeb3Contract({
+      abi: hamsterNftAbi,
+      contractAddress: hamsterNftAddress,
+      functionName: "ownerOf",
+      params: { tokenId },
+    });
+
+    return runContractFunction();
+  }
 
   async function handleHamsterSelect(tokenId) {
     setSelectedHamsterTokenId(tokenId);
