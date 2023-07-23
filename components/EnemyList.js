796af9f8986a61023dd427f9730fba8cf87adba1
BuildTools <unconfigured@null.spigotmc.org>
2023-07-24 00:25:47 +0200

    small fixes 41

--- components/EnemyList.js
+++ components/EnemyList.js
@@ -1,80 +1,62 @@
-import { useState, useEffect } from "react";
-import { useWeb3Contract, useMoralis } from "react-moralis";
-import { useHamsterContext } from "./HamsterContext";
-import { Dropdown, Avatar } from "web3uikit";
-import hamsterNftAbi from "../constants/BasicNft.json";
+import React, { useState, useEffect } from 'react';
+import { useMoralis } from 'react-moralis';
 
 const hamsterNftAddress = "0x5726c14663a1ead4a7d320e8a653c9710b2a2e89";
+import hamsterNftAbi from "../constants/BasicNft.json";
 
-export default function EnemyList() {
-  const { isWeb3Enabled, account } = useMoralis();
-  const { selectedHamsterTokenId } = useHamsterContext();
-  const [enemyList, setEnemyList] = useState([]);
+const EnemyList = () => {
+  const [tokenId, setTokenId] = useState(0);
+  const { user, Moralis } = useMoralis();
+  const maxTokenId = 99;
 
-  useEffect(() => {
-    if (isWeb3Enabled && account && selectedHamsterTokenId !== null) {
-      updateEnemyList();
-    }
-  }, [isWeb3Enabled, account, selectedHamsterTokenId]);
+  // Function to fetch the enemy NFTs not owned by the zero address and not owned by the connected account
+  const fetchEnemyNFTs = async () => {
+    try {
+      const contract = new Moralis.web3.eth.Contract(hamsterNftAbi, hamsterNftAddress);
+      const enemyNFTs = [];
 
-  async function updateEnemyList() {
-    const contract = useWeb3Contract({
-      abi: hamsterNftAbi,
-      contractAddress: hamsterNftAddress,
-      functionName: "ownerOf",
-    });
-
-    const newEnemyList = [];
-    for (let i = 0; i < 100; i++) {
-      try {
-        const owner = await contract.call({ tokenId: i });
-        console.log(`Hamster #${i} Owner: ${owner}`);
-        if (owner !== account && owner !== "0x0000000000000000000000000000000000000000") {
-          newEnemyList.push({
-            id: i,
-            label: `#${i}`,
-            prefix: (
-              <Avatar
-                avatarKey={3}
-                borderRadius={7.5}
-                fontSize={8}
-                size={24}
-                image={`../images/nobg/${i}.png`}
-                theme="image"
-              />
-            ),
-          });
+      for (let i = 0; i <= maxTokenId; i++) {
+        const owner = await contract.methods.ownerOf(i).call();
+        if (owner.toLowerCase() !== '0x0000000000000000000000000000000000000000' && owner.toLowerCase() !== user.attributes.ethAddress.toLowerCase()) {
+          enemyNFTs.push({ tokenId: i });
         }
-      } catch (error) {
-        console.error(`Error fetching owner of Hamster #${i}`, error);
       }
-    }
-    setEnemyList(newEnemyList);
-    console.log("Enemy List:", newEnemyList);
-  }
 
-  const [selectedEnemyTokenId, setSelectedEnemyTokenId] = useState(null);
-  async function handleEnemySelect(tokenId) {
-    setSelectedEnemyTokenId(tokenId);
-  }
+      if (enemyNFTs.length > 0) {
+        setTokenId(enemyNFTs[0].tokenId);
+      }
+    } catch (error) {
+      console.error('Error fetching enemy NFTs:', error);
+    }
+  };
+
+  // Fetch enemy NFTs on component mount or when the user changes
+  useEffect(() => {
+    if (user) {
+      fetchEnemyNFTs();
+    }
+  }, [user]);
+
+  // Function to handle dropdown selection change
+  const handleTokenChange = (event) => {
+    const selectedTokenId = parseInt(event.target.value, 10);
+    setTokenId(selectedTokenId);
+  };
 
   return (
     <div>
-      <div>
-        {account ? (
-          <div>
-            <div>Select which Hamster to challenge!</div>
-            <Dropdown
-              onChange={(e) => handleEnemySelect(e.id)}
-              id="inputEnemy"
-              label="Hamster: "
-              options={enemyList}
-            />
-          </div>
-        ) : (
-          <div>Connect your wallet...</div>
-        )}
-      </div>
+      <label htmlFor="enemyTokenSelect">Select an Enemy NFT:</label>
+      <select id="enemyTokenSelect" value={tokenId} onChange={handleTokenChange}>
+        {/* Render dropdown options for enemy tokenIds not owned by zero address and connected account */}
+        {Array.from({ length: maxTokenId + 1 }).map((_, index) => (
+          <option key={index} value={index}>
+            Enemy NFT {index}
+          </option>
+        ))}
+      </select>
+      <p>Selected Enemy Token ID: {tokenId}</p>
     </div>
   );
-}
+};
+
+export default EnemyList;
--- components/HamsterList.js
+++ components/HamsterList.js
@@ -1,79 +1,62 @@
-import { useState, useEffect } from "react";
-import { useWeb3Contract, useMoralis } from "react-moralis";
-import { useHamsterContext } from "./HamsterContext";
-import { Dropdown, Avatar } from "web3uikit";
-import hamsterNftAbi from "../constants/BasicNft.json";
+import React, { useState, useEffect } from 'react';
+import { useMoralis } from 'react-moralis';
 
 const hamsterNftAddress = "0x5726c14663a1ead4a7d320e8a653c9710b2a2e89";
+import hamsterNftAbi from "../constants/BasicNft.json";
 
-export default function HamsterList() {
-  const { selectedHamsterTokenId, setSelectedHamsterTokenId } = useHamsterContext();
-  const { isWeb3Enabled, account } = useMoralis();
-  const [hamsterList, setHamsterList] = useState([]);
+const HamsterList = () => {
+  const [tokenId, setTokenId] = useState(0);
+  const { user, Moralis } = useMoralis();
+  const maxTokenId = 99;
 
-  useEffect(() => {
-    if (isWeb3Enabled && account) {
-      updateHamsterList();
-    }
-  }, [isWeb3Enabled, account]);
+  // Function to fetch the NFTs owned by the connected account
+  const fetchNFTs = async () => {
+    try {
+      const contract = new Moralis.web3.eth.Contract(hamsterNftAbi, hamsterNftAddress);
+      const ownedNFTs = [];
 
-  async function updateHamsterList() {
-    const contract = useWeb3Contract({
-      abi: hamsterNftAbi,
-      contractAddress: hamsterNftAddress,
-      functionName: "ownerOf",
-    });
-
-    const newHamsterList = [];
-    for (let i = 0; i < 100; i++) {
-      try {
-        const owner = await contract.call({ tokenId: i });
-        console.log(`Hamster #${i} Owner: ${owner}`);
-        if (owner === account) {
-          newHamsterList.push({
-            id: i,
-            label: `#${i}`,
-            prefix: (
-              <Avatar
-                avatarKey={3}
-                borderRadius={7.5}
-                fontSize={8}
-                size={24}
-                image={`../images/nobg/${i}.png`}
-                theme="image"
-              />
-            ),
-          });
+      for (let i = 0; i <= maxTokenId; i++) {
+        const owner = await contract.methods.ownerOf(i).call();
+        if (owner.toLowerCase() === user.attributes.ethAddress.toLowerCase()) {
+          ownedNFTs.push({ tokenId: i });
         }
-      } catch (error) {
-        console.error(`Error fetching owner of Hamster #${i}`, error);
       }
-    }
-    setHamsterList(newHamsterList);
-    console.log("Hamster List:", newHamsterList);
-  }
 
-  async function handleHamsterSelect(tokenId) {
-    setSelectedHamsterTokenId(tokenId);
-  }
+      if (ownedNFTs.length > 0) {
+        setTokenId(ownedNFTs[0].tokenId);
+      }
+    } catch (error) {
+      console.error('Error fetching NFTs:', error);
+    }
+  };
+
+  // Fetch NFTs on component mount
+  useEffect(() => {
+    if (user) {
+      fetchNFTs();
+    }
+  }, [user]);
+
+  // Function to handle dropdown selection change
+  const handleTokenChange = (event) => {
+    const selectedTokenId = parseInt(event.target.value, 10);
+    setTokenId(selectedTokenId);
+  };
 
   return (
     <div>
-      <div>
-        {account ? (
-          <div>
-            <div>Select your Hamster to race!</div>
-            <Dropdown
-              onChange={(e) => handleHamsterSelect(e.id)}
-              id="inputHamster"
-              label="Hamster: "
-              options={hamsterList}
-            />
-          </div>
-        ) : (
-          <div>Connect your wallet...</div>
-        )}
-      </div>
+      <label htmlFor="tokenSelect">Select an NFT:</label>
+      <select id="tokenSelect" value={tokenId} onChange={handleTokenChange}>
+        {/* Render dropdown options for tokenId 0 to maxTokenId */}
+        {Array.from({ length: maxTokenId + 1 }).map((_, index) => (
+          <option key={index} value={index}>
+            NFT {index}
+          </option>
+        ))}
+      </select>
+      <p>Selected Token ID: {tokenId}</p>
     </div>
   );
-}
+};
+
+export default HamsterList;
