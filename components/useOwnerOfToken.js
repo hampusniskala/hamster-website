d09789d730e34fcd0bacf19e0409a1ac15dd128b
BuildTools <unconfigured@null.spigotmc.org>
2023-07-24 14:38:21 +0200

    small changes 79

--- components/useOwnerOfToken.js
+++ components/useOwnerOfToken.js
@@ -1,24 +1,23 @@
 // useOwnerOfToken.js
+
 import { useWeb3Contract } from 'react-moralis';
 import hamsterNftAbi from '../constants/BasicNft.json';
 
 const hamsterNftAddress = '0x5726c14663a1ead4a7d320e8a653c9710b2a2e89';
 
 const useOwnerOfToken = (tokenId) => {
-  const { user } = useMoralis();
-
-  const { data, error } = useWeb3Contract({
+  const { contract, contractState } = useWeb3Contract({
     abi: hamsterNftAbi,
     contractAddress: hamsterNftAddress,
-    functionName: 'ownerOf',
-    params: {
-      tokenId: tokenId,
-    },
   });
 
+  // Fetch the owner synchronously
+  const owner = contractState?.value.ownerOf[tokenId] || null;
+
   return {
-    owner: data || null,
-    error,
+    owner,
+    contract,
+    contractState,
   };
 };
 
