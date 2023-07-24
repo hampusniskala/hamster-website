1959ee3e3841d4c4069b85b0c3824f973afd5543
BuildTools <unconfigured@null.spigotmc.org>
2023-07-24 12:10:03 +0200

    small fixes 51

--- components/HamsterSelector.js
+++ components/HamsterSelector.js
@@ -1,8 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import { useWeb3Contract, useMoralis } from 'react-moralis';
 import hamsterNftAbi from '../constants/BasicNft.json';
+import Image from 'next/image';
 
 const hamsterNftAddress = '0x5726c14663a1ead4a7d320e8a653c9710b2a2e89';
+
+// Custom React Hook to fetch owned hamsters
+const useOwnedHamsters = () => {
+  const { user } = useMoralis();
+  const [ownedHamsters, setOwnedHamsters] = useState([]);
+
+  useEffect(() => {
+    const fetchOwnedHamsters = async () => {
+      const contract = useWeb3Contract(hamsterNftAddress, hamsterNftAbi);
+      const ownedHamsters = [];
+      for (let tokenId = 0; tokenId < 100; tokenId++) {
+        const owner = await contract.methods.ownerOf(tokenId).call();
+        if (owner === user.attributes.ethAddress) {
+          ownedHamsters.push({ tokenId });
+        }
+      }
+      setOwnedHamsters(ownedHamsters);
+    };
+
+    fetchOwnedHamsters();
+  }, [user]);
+
+  return ownedHamsters;
+};
+
+// Custom React Hook to fetch all hamsters
+const useAllHamsters = () => {
+  const { user } = useMoralis();
+  const [allHamsters, setAllHamsters] = useState([]);
+
+  useEffect(() => {
+    const fetchAllHamsters = async () => {
+      const contract = useWeb3Contract(hamsterNftAddress, hamsterNftAbi);
+      const allHamsters = [];
+      for (let tokenId = 0; tokenId < 100; tokenId++) {
+        const owner = await contract.methods.ownerOf(tokenId).call();
+        if (owner !== user.attributes.ethAddress && owner !== '0x0000000000000000000000000000000000000000') {
+          allHamsters.push({ tokenId });
+        }
+      }
+      setAllHamsters(allHamsters);
+    };
+
+    fetchAllHamsters();
+  }, [user]);
+
+  return allHamsters;
+};
 
 const HamsterSelector = ({ hamsters, onChange }) => {
   return (
@@ -37,57 +86,29 @@ const EnemySelector = ({ hamsters, onChange }) => {
 };
 
 const HamsterPage = () => {
-  const { user } = useMoralis();
-  const [ownedHamsters, setOwnedHamsters] = useState([]);
-  const [allHamsters, setAllHamsters] = useState([]);
+  const ownedHamsters = useOwnedHamsters();
+  const allHamsters = useAllHamsters();
   const [selectedHamster, setSelectedHamster] = useState('');
   const [selectedEnemy, setSelectedEnemy] = useState('');
-
-  useEffect(() => {
-    // Fetch all the NFTs owned by the connected account with tokenIds 0 to 99
-    const fetchOwnedHamsters = async () => {
-      const contract = useWeb3Contract(hamsterNftAddress, hamsterNftAbi);
-      const ownedHamsters = [];
-      for (let tokenId = 0; tokenId < 100; tokenId++) {
-        const owner = await contract.methods.ownerOf(tokenId).call();
-        if (owner === user.attributes.ethAddress) {
-          ownedHamsters.push({ tokenId });
-        }
-      }
-      setOwnedHamsters(ownedHamsters);
-    };
-
-    // Fetch all the NFTs with tokenIds 0 to 99 that are not owned by the connected account
-    const fetchAllHamsters = async () => {
-      const contract = useWeb3Contract(hamsterNftAddress, hamsterNftAbi);
-      const allHamsters = [];
-      for (let tokenId = 0; tokenId < 100; tokenId++) {
-        const owner = await contract.methods.ownerOf(tokenId).call();
-        if (owner !== user.attributes.ethAddress && owner !== '0x0000000000000000000000000000000000000000') {
-          allHamsters.push({ tokenId });
-        }
-      }
-      setAllHamsters(allHamsters);
-    };
-
-    fetchOwnedHamsters();
-    fetchAllHamsters();
-  }, [user]);
 
   return (
     <div>
       <HamsterSelector hamsters={ownedHamsters} onChange={setSelectedHamster} />
       {selectedHamster && (
-        <img
-          src={`../images/nobg/${selectedHamster}.png`}
+        <Image
+          src={`/images/nobg/${selectedHamster}.png`}
           alt={`Hamster ${selectedHamster}`}
+          width={200}
+          height={200}
         />
       )}
       <EnemySelector hamsters={allHamsters} onChange={setSelectedEnemy} />
       {selectedEnemy && (
-        <img
-          src={`../images/nobg/${selectedEnemy}.png`}
+        <Image
+          src={`/images/nobg/${selectedEnemy}.png`}
           alt={`Enemy ${selectedEnemy}`}
+          width={200}
+          height={200}
         />
       )}
     </div>
