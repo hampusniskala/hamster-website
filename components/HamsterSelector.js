cb090917c170e6ed8fc5cfba3998f76e0428fd2e
BuildTools <unconfigured@null.spigotmc.org>
2023-07-24 14:54:15 +0200

    small changes 85

--- components/HamsterSelector.js
+++ components/HamsterSelector.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useMoralis, useWeb3Contract } from 'react-moralis';
+import useOwnerOfToken from './useOwnerOfToken';
 
 import hamsterNftAbi from '../constants/BasicNft.json';
 
@@ -15,24 +16,21 @@ const HamsterPage = () => {
     const fetchOwnedTokenIds = async () => {
       if (isWeb3Enabled) {
           console.log("Web3 detected")
-        // Create a Web3 contract instance
-        const contract = new Moralis.web3.eth.Contract(hamsterNftAbi, hamsterNftAddress);
-        console.log("contract: ", contract)
 
         // Get the connected wallet address
         const userAddress = user.get('ethAddress');
         console.log("address: ", userAddress)
 
         // Get the total supply of tokens
-        const totalSupply = await contract.methods.totalSupply().call();
+        const totalSupply = 100
         console.log("totalSupply: ", totalSupply)
 
         // Fetch the token IDs owned by the user
         const ownedTokenIds = [];
 
         for (let i = 0; i < totalSupply; i++) {
-          const tokenId = await contract.methods.tokenByIndex(i).call();
-          const owner = await contract.methods.ownerOf(tokenId).call();
+          const tokenId = i
+          const owner = useOwnerOfToken(tokenId)
 
           if (owner.toLowerCase() === userAddress.toLowerCase()) {
             ownedTokenIds.push(tokenId);
