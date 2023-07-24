c0fa18f98fc537c169a3328e63b9e5753fcd3437
BuildTools <unconfigured@null.spigotmc.org>
2023-07-24 16:42:07 +0200

    small changes 91

--- components/useOwnerOfToken.js
+++ components/useOwnerOfToken.js
@@ -6,18 +6,20 @@ import hamsterNftAbi from '../constants/BasicNft.json';
 const hamsterNftAddress = '0x5726c14663a1ead4a7d320e8a653c9710b2a2e89';
 
 const useOwnerOfToken = (tokenId) => {
-  const { contract, contractState } = useWeb3Contract({
-    abi: hamsterNftAbi,
-    contractAddress: hamsterNftAddress,
-  });
+    const { runContractFunction: ownerOf } = useWeb3Contract({
+        abi: hamsterNftAbi,
+        contractAddress: hamsterNftAddress,
+        functionName: "ownerOf",
+        params: {
+            tokenId: tokenId,
+        },
+    })
 
   // Fetch the owner synchronously
-  const owner = contractState?.value.ownerOf[tokenId] || null;
+  const owner = await ownerOf(tokenId)
 
   return {
-    owner,
-    contract,
-    contractState,
+    owner
   };
 };
 
